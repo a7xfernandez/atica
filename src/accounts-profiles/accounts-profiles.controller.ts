fc272c9e8c99b179765883f306262cd82c17e368
Esaud Rivera Lagos <Esaud17@users.noreply.github.com>
2021-08-04 21:12:52 -0600

    security

--- src/accounts-profiles/accounts-profiles.controller.ts
+++ src/accounts-profiles/accounts-profiles.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { AccountsProfileDto } from './dto/accounts-profile.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { AccountsProfilesService } from './services/accounts-profiles.service';
 import { CreateAccountsProfileDto } from './dto/create-accounts-profile.dto';
 import { UpdateAccountsProfileDto } from './dto/update-accounts-profile.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiTags('accounts-profiles')
 @Controller('accounts-profiles')
@@ -11,11 +13,29 @@ export class AccountsProfilesController {
     private readonly accountsProfilesService: AccountsProfilesService,
   ) {}
 
+  @ApiBody({})
+  @ApiResponse({
+    status: 200,
+    type: AccountsProfileDto,
+    description: 'procesado correctamente',
+  })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.accountsProfilesService.findOne(+id);
   }
 
+  @ApiBody({ type: UpdateAccountsProfileDto })
+  @ApiResponse({
+    status: 200,
+    type: AccountsProfileDto,
+    description: 'procesado correctamente',
+  })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -24,6 +44,15 @@ export class AccountsProfilesController {
     return this.accountsProfilesService.update(+id, updateAccountsProfileDto);
   }
 
+  @ApiBody({})
+  @ApiResponse({
+    status: 200,
+    type: AccountsProfileDto,
+    description: 'procesado correctamente',
+  })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.accountsProfilesService.remove(+id);
